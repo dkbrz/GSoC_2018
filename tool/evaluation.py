33d6b15e7fb4673c9a9626ac5d4db6903e060d46
dkbrz <glaz.dikobraz@gmail.com>
2018-06-30 17:44:17 +0300

    finished language ranking

--- tool/evaluation.py
+++ tool/evaluation.py
@@ -1,4 +1,6 @@
-def node_search(node, l2, cutoff):
+from .functions import *
+
+def node_search(G, node, l2, cutoff):
     if node not in G.nodes():
         return None
     s = FilteredList(nx.single_source_shortest_path_length(G, node, cutoff=cutoff))
@@ -21,20 +23,19 @@ def node_search(node, l2, cutoff):
             for i in t: coef += exp(-t[i])
             results[translation] = coef
     return list(sorted(results, key=results.get, reverse=True))
-	
+    
 def two_node_search (G, node1, node2, l1, l2, cutoff):
     if (node1, node2) in G.edges(): G.remove_edge(node1, node2)
     if (node2, node1) in G.edges(): G.remove_edge(node2, node1)
-    res1 = node_search(node1, l2, cutoff)
-    res2 = node_search(node2, l1, cutoff)
+    res1 = node_search(G, node1, l2, cutoff)
+    res2 = node_search(G, node2, l1, cutoff)
     coefficient = 0
     if node2 in res1: coefficient += 0.5*(len(res1) - res1.index(node2))/len(res1)
     if node1 in res2: coefficient += 0.5*(len(res2) - res2.index(node1))/len(res2) 
     return coefficient
-	
+    
 def evaluate(G, pairs, l1, l2, cutoff):
     result = []
     for i in pairs:
         result.append(two_node_search (G, i[0], i[1], l1, l2, cutoff))
     return result
-	
