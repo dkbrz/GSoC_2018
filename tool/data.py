c243b67033d17a6687cf4d3e306706fe0a9f018a
dkbrz <glaz.dikobraz@gmail.com>
2018-07-12 13:07:27 +0300

    example

---
+++ tool/data.py
@@ -0,0 +1,6 @@
+rename = {'epo-bul':'bul-epo', 'epo-pes':'pes-epo', 'epo-pol':'pol-epo', 'epo-bel':'bel-epo','epo-bre':'bre-epo',
+          'epo-ces':'ces-epo','epo-deu':'deu-epo', 'epo-ell':'ell-epo','epo-fin':'fin-epo','spa-qve':'qve-spa',
+          'hun-eng':'eng-hun'}
+remove = ['ces-ces.dix','ita-ita.dix', 'eus-eu_bis.dix','hbs_HR-hbs_SR.dix','zh_CN-zh_TW.dix']
+
+variants = {'nor':['nno','nob'], 'cat':['val']}
